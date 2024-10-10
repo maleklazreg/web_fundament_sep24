d50e64bed15594c028daec47360ff9982bce2a4f
malek lazreg <lazregmalek30@gmail.com>
2024-10-10 11:25:43 +0100

    Making Connections

---
+++ Week02/Day02/Practice/Making Connections/script.js
@@ -0,0 +1,10 @@
+function changename(jone){
+    var elemnt = document.querySelector(jone)
+    elemnt.innerText = "Alexa Doe";
+}
+function removeuser1(x){
+    document.querySelector(x).remove()
+}
+function removeuser2(x){
+    document.querySelector(x).remove()
+}
